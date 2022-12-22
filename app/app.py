ea406cd6ad5cda8ff290b4279486e522e2d7b744
Guy B <91497242+guyb2022@users.noreply.github.com>
2022-12-22 13:43:21 +0200

    Update app.py

--- app/app.py
+++ app/app.py
@@ -13,7 +13,7 @@ def homepage(): # Redirecting to home page
     return render_template("index.html")
 
 if __name__ == '__main__':
-    app.run(debug=True, host="0.0.0.0", port=8080)
+    app.run(debug=True, host="0.0.0.0", port=int(os.environ.get('PORT', 8080)))
 
 
 
